66cfce6dd02f026702336ea427a4c8ad58257ee4
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-10-01 12:14:37 +0300

    Express, handlebars main structure files created,

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.static('./src/public'));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.listen(PORT, () => console.log('Server is listening'))
