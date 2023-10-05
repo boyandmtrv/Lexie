2c26f08a78e61c4464da993eaf31f83171eb94a2
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-10-05 15:09:37 +0300

    User login validation

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,4 +1,22 @@
+const bCrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Cannot find username or password');
+    };
+
+    const isValid = await bCrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Cannot find username or password');
+    };
+
+    return user;
+};
 
