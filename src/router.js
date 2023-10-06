bfe1bccb3d845e186a08a844f8ec4455258145e0
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-10-06 11:24:59 +0300

    Static register page created

--- src/router.js
+++ src/router.js
@@ -5,12 +5,14 @@ const dashboardController = require('./controllers/dashboardController');
 const createContoller = require('./controllers/createConroller');
 const noteContoller = require('./controllers/noteController');
 const recordController = require('./controllers/recordsContoller');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use(dashboardController);
 router.use(createContoller);
 router.use('/notes', noteContoller);
 router.use(recordController);
+router.use('/users', userController);
 
 router.get('*', (req, res) => {
     res.render('404');
