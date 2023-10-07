2f4811abab98c595b76ce386681a47e579822f54
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-10-07 13:33:14 +0300

    Dashboard nav bar re-styled and for now it is ok

--- src/router.js
+++ src/router.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const dashboardController = require('./controllers/dashboardController');
-const createContoller = require('./controllers/createConroller');
 const noteContoller = require('./controllers/noteController');
 const recordController = require('./controllers/recordsContoller');
 const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use(dashboardController);
-router.use(createContoller);
 router.use('/notes', noteContoller);
 router.use(recordController);
 router.use('/users', userController);
