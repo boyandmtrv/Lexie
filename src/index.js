4f0948242314414153637e9448cc86bdae723be1
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-10-25 20:16:24 +0300

    minor change

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-// const dbConnect = require('./config/dbConfig');
+const dbConnect = require('./config/dbConfig');
 const errorHandler = require('./middlewares/errorMiddleware');
 
 const routes = require('./router');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;;
 
 expressConfig(app);
 handlebarsConfig(app);
 
-// dbConnect()
-//     .then(() => console.log('DB Connected Successfully'))
-//     .catch(err => console.log(`DB error: ${err}`));
-
-mongoose.set('strictQuery', false);
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-};
+dbConnect()
+    .then(() => console.log('DB Connected Successfully'))
+    .catch(err => console.log(`DB error: ${err}`));
 
 app.use(routes);
 app.use(errorHandler);
 
-connectDB()
-.then(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
