43b2ebf0a33f5e584b73935e3c328a3129b84011
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-10-26 11:42:25 +0300

    Prep for deploy

--- back-end/config/expressConfig.js
+++ back-end/config/expressConfig.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const { auth } = require('../middlewares/authMiddleware');
 
 function expressConfig(app) {
-    app.use(express.static(path.resolve(__dirname, '../public')));
+    app.use(express.static(path.resolve(__dirname, '../../front-end/public')));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(auth);
--- back-end/config/handlebarsConfig.js
+++ back-end/config/handlebarsConfig.js
@@ -6,7 +6,7 @@ function handlebarsConfig(app) {
     }));
     
     app.set('view engine', 'hbs');
-    app.set('views', 'src/views');
+    app.set('views', 'front-end/views');
 };
 
 module.exports = handlebarsConfig;
